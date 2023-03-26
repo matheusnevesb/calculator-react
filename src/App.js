ae5afab87da04cda419dab199541902366ca25bf
matheus <matheusnbbg@hotmail.com>
2023-03-26 00:24:15 -0300

    adicionado funcoes handleReset e handleEquations

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useState } from 'react';
 
 function App() {
   const [display, setDisplay] = useState(0)
+  const [stbyDisplay, setStbyDisplay] = useState('')
+  const [equation, setEquation] = useState('')
   // const bts = ['7','8', '9', 'DEL', '4', '5', '6', '+', '1', '2', '3', '-', '.', '0', '/', 'x', 'reset', '=']
   function handleNumbers(target) {
     const numero = target.innerText;
@@ -11,6 +13,14 @@ function App() {
       setDisplay(display + numero);
     }
   }
+  function handleReset(){
+    setDisplay(0)
+  }
+  function handleEquations(target) {
+    setStbyDisplay(display);
+    setDisplay(0);
+    setEquation(target.innerText)
+  }
   return (
     <div>
       <div>
@@ -19,6 +29,7 @@ function App() {
         <button>2</button>
         <button>3</button>
       </div>
+      <div>{`${stbyDisplay} ${equation}`}</div>
       <div>{display}</div>
     {/* {bts.map((botao) => (
       <button
@@ -38,13 +49,13 @@ function App() {
         <button onClick={({target}) => handleNumbers(target)}>4</button>
         <button onClick={({target}) => handleNumbers(target)}>5</button>
         <button onClick={({target}) => handleNumbers(target)}>6</button>
-        <button>+</button>
+        <button onClick={({target}) => handleEquations(target)}>+</button>
       </div>
       <div>
         <button onClick={({target}) => handleNumbers(target)}>1</button>
         <button onClick={({target}) => handleNumbers(target)}>2</button>
         <button onClick={({target}) => handleNumbers(target)}>3</button>
-        <button>-</button>
+        <button onClick={({target}) => handleEquations(target)}>-</button>
       </div>
       <div>
         <button>.</button>
@@ -53,7 +64,7 @@ function App() {
         <button>x</button>
       </div>
       <div>
-        <button>Reset</button>
+        <button onClick={() => handleReset()}>Reset</button>
         <button>=</button>
       </div>
     </div>

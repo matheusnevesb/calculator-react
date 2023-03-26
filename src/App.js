3ed0c85612d0009de074f07d4b54a97f0e37f0c3
matheus <matheusnbbg@hotmail.com>
2023-03-26 00:12:20 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [display, setDisplay] = useState(0)
+  // const bts = ['7','8', '9', 'DEL', '4', '5', '6', '+', '1', '2', '3', '-', '.', '0', '/', 'x', 'reset', '=']
+  function handleNumbers(target) {
+    const numero = target.innerText;
+    if (Number(display) === 0 ) {
+      setDisplay(numero);
+    }else {
+      setDisplay(display + numero);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <p>Calculator</p>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+      </div>
+      <div>{display}</div>
+    {/* {bts.map((botao) => (
+      <button
+        onClick={({target}) => handleNumbers(target)}
+        key={botao}
+      >
+        {botao}
+      </button>
+    ))} */}
+      <div>
+        <button onClick={({target}) => handleNumbers(target)}>7</button>
+        <button onClick={({target}) => handleNumbers(target)}>8</button>
+        <button onClick={({target}) => handleNumbers(target)}>9</button>
+        <button>DEL</button>
+      </div>
+      <div>
+        <button onClick={({target}) => handleNumbers(target)}>4</button>
+        <button onClick={({target}) => handleNumbers(target)}>5</button>
+        <button onClick={({target}) => handleNumbers(target)}>6</button>
+        <button>+</button>
+      </div>
+      <div>
+        <button onClick={({target}) => handleNumbers(target)}>1</button>
+        <button onClick={({target}) => handleNumbers(target)}>2</button>
+        <button onClick={({target}) => handleNumbers(target)}>3</button>
+        <button>-</button>
+      </div>
+      <div>
+        <button>.</button>
+        <button onClick={({target}) => handleNumbers(target)}>0</button>
+        <button>/</button>
+        <button>x</button>
+      </div>
+      <div>
+        <button>Reset</button>
+        <button>=</button>
+      </div>
     </div>
   );
 }
